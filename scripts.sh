9681633c1ad5812890a91a344ea9720203fa0304
rgurgul <gurgul.robert@gmail.com>
2019-07-23 11:01:52 +0200

    siedem

--- scripts.sh
+++ scripts.sh
@@ -1,7 +1,10 @@
-git add .
-git commit -m $1
-git tag v$2
-git push origin master
-git push --tags origin
-
-#echo "branch $1 cloned to $2"
+if [ $1 ] && [ $2 ]; then
+    git add .
+    git commit -m $1
+    git tag v$2
+    git push origin master
+    git push --tags origin
+    npm publish
+else
+    echo 'commit name or version missing!'
+fi
