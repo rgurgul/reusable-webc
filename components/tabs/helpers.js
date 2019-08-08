339263fc23a4c14ae635bcb8210057ca25b22327
rgurgul <gurgul.robert@gmail.com>
2019-08-08 13:24:27 +0200

    tabs

---
+++ components/tabs/helpers.js
@@ -0,0 +1,26 @@
+export default function Helpers() {
+}
+
+Helpers.createEl = function (tagName, target, params) {
+    var el = document.createElement(tagName);
+    if (params) {
+        Object.keys(params).forEach(function (key) {
+            if (key in el) {
+                el[key] = params[key];
+            } else {
+                el.setAttribute(key, params[key]);
+            }
+        })
+    }
+    target instanceof HTMLElement
+        ? target.appendChild(el)
+        : document.querySelector(target).appendChild(el);
+
+    return el;
+};
+
+Helpers.getHtmlTmpl = async function (url) {
+    const res = await fetch(url);
+    const textTemplate = await res.text();
+    return new DOMParser().parseFromString(textTemplate, 'text/html').querySelector('template');
+}
---
+++ components/tabs/tabs.js
@@ -0,0 +1,65 @@
+import Helpers from "./helpers.js";
+
+(function () {
+
+    class UiTabs extends HTMLElement {
+        async  connectedCallback() {
+            const root = this.attachShadow({ mode: 'open' });
+            const tpl = await Helpers.getHtmlTmpl('components/tabs/tabs.html');
+            root.appendChild(tpl.content.cloneNode(true));
+            this.createButtons(this);
+        }
+
+        setActive(btn, tab) {
+            try {
+                this
+                    .querySelector('ui-tab[visible=true]')
+                    .setAttribute('visible', 'false');
+                this
+                    .querySelector('button.btn-active')
+                    .classList.remove('btn-active');
+            } catch (err) { }
+
+            tab.setAttribute('visible', 'true');
+            btn.classList.add('btn-active');
+        }
+
+        createButtons() {
+            Array.from(this.querySelectorAll('ui-tab'))
+                .forEach((tab, idx) => {
+                    const slotType = tab.getAttribute('type');
+                    if (!this.shadowRoot.querySelector(`slot[name=${slotType}]`)) {
+                        const label = this.shadowRoot.querySelector('.btns').appendChild(document.createElement('div'));
+                        label.innerHTML = slotType;
+                        label.style = `font-weight: bold; text-transform:uppercase`
+                        const newSlot = this.shadowRoot.querySelector('.btns').appendChild(document.createElement('slot'));
+                        newSlot.name = slotType;
+                    }
+                    const tabName = tab.getAttribute('tab-name');
+                    const btn = Helpers.createEl('button', this,
+                        {
+                            'data-target': tabName,
+                            innerHTML: `<span style="background: white; color: black; padding: 2px 3px; margin: 0 2px;"><b>${idx}</b></span> ${tabName}`,
+                            slot: slotType,
+                            className: 'link'
+                        });
+                    tab.getAttribute('visible') && (btn.className = 'btn-active');
+                    btn.addEventListener('click', (evt) => {
+                        this.setActive(evt.currentTarget, tab);
+                    });
+                });
+        }
+    }
+
+    class UiTab extends HTMLElement {
+        async connectedCallback() {
+            const root = this.attachShadow({ mode: 'open' });
+            const tpl = await Helpers.getHtmlTmpl('components/tabs/tab.html');
+            root.appendChild(tpl.content.cloneNode(true));
+        }
+    }
+
+    customElements.define('ui-tabs', UiTabs);
+    customElements.define('ui-tab', UiTab);
+
+}());
