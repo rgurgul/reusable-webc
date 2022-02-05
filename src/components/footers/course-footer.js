63b15f82fb3a7ab708dde0e0ccecbb980ec3ba3f
rgurgul <gurgul.robert@gmail.com>
2022-02-05 21:21:51 +0100

    ref

--- src/components/footers/course-footer.js
+++ src/components/footers/course-footer.js
@@ -1,12 +1,12 @@
 class CourseFooter extends HTMLElement {
-    static get observedAttributes() {
-        return ['courses'];
-    }
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
+  static get observedAttributes() {
+    return ["courses"];
+  }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
 
-        this.shadowRoot.innerHTML = `
+    this.shadowRoot.innerHTML = `
             <footer>
                 <div class="linkedin">
                     <h3>Podziel się opinią o szkoleniu</h3>
@@ -22,25 +22,20 @@ class CourseFooter extends HTMLElement {
             </footer>
          `;
 
-        const style = document.createElement('style')
-        style.textContent = `
+    const style = document.createElement("style");
+    style.textContent = `
         footer{
-            border: 2px solid #eaeaea;
             border-radius: 4px;
-            padding: 20px;
             display: flex;
-            flex-wrap: wrap;
+            gap:20px;
         }
         footer a {
-            font-weight: 600;
             cursor: pointer;
-            text-decoration: none;
-            border: 2px solid slategray;
-            border-radius: 2px;
-            color: #555;
-            padding: 2px 6px;
-            margin: 0;
+            color: black;
             display: inline-block;
+        }
+        footer a:hover {
+            color: coral;
         }
         footer ul {
             margin: 0;
@@ -58,54 +53,103 @@ class CourseFooter extends HTMLElement {
         *{
             font-family: sans-serif;
         }
-        .linkedin, .courses{
-            flex: 1;
+        .linkedin,.list{
+            border: 2px solid #eaeaea;
+            padding:20px;
+            margin: 20px 0;
+            border-radius: 4px;
+            box-shadow: 0 5px 10px  #eaeaea;
+            background: rgba(0,0,0,.01);
         }
-        @media screen and (max-width: 600px) {
+        .courses {
+            flex:1;
+        }
+        .flexi {
+            display: flex;
+            gap:40px;
+        }
+        dt {
+            text-transform: uppercase;
+            font-weight: bold;
+        }
+        dd {
+            margin-left: 0;
+        }
+        @media screen and (max-width: 768px) {
+            .flexi {
+                display: block;
+            }
             footer {
                 flex-direction: column;
             }
-            .linkedin, .courses {
+            .linkedin, .list {
                 flex-basis: auto;
                 margin-top: 20px;
             }
         }
         `;
-        this.shadowRoot.appendChild(style);
-    }
+    this.shadowRoot.appendChild(style);
+  }
 
-    courses() {
-        const container = document.createElement('div');
-        container.classList.add('courses');
-        this.shadowRoot.querySelector('footer').appendChild(container);
-        fetch('https://panel.debugger.pl/api/courses')
-            .then((resp) => resp.json())
-            .then((resp) => {
-                container.innerHTML = `
+  courses() {
+    const container = document.createElement("div");
+    container.classList.add("courses");
+    this.shadowRoot.querySelector("footer").appendChild(container);
+    fetch("https://panel.debugger.pl/api/courses")
+      .then((resp) => resp.json())
+      .then((resp) => {
+        const xx = resp.reduce(
+          (acc, val) => ({ ...acc, [val.type]: [...acc[val.type], val] }),
+          { "front-end": [], "back-end": [], inne: [] }
+        );
 
-                <div class="courses">
+        const rr = Object.entries(xx);
+
+        container.innerHTML = `
+                <div class="list">
                     <h3>Lista wszystkich szkoleń</h3>
-                    <br>
-                    <ul>
-                    ${resp.map((item, idx) => `
-                        <li>
-                            <a href='https://debugger.pl/szkolenie-${item.name}' target='_blank'>
-                            ${item.name.charAt(0).toUpperCase() + item.name.slice(1).replace(/-[\w]/g, (val, idx, str) => {
-                                const result = str.charAt(idx + 2) !== '-' ? val.charAt(1).toUpperCase() : val.charAt(1);
-                                return ' ' + result;
-                            })}</a>
-                        </li>
-                    `).join('')}
-                    </ul>
+<div class="flexi">
+                    ${rr
+                      .map(
+                        (col) => `
+
+                    <dl>
+                            <dt>${col[0]}</dt>
+                    ${col[1]
+                      .map(
+                        (item, idx) => `
+                        <dd>
+                            <a href='https://debugger.pl/szkolenie-${
+                              item.name
+                            }' target='_blank'>
+                            ${
+                              item.name.charAt(0).toUpperCase() +
+                              item.name
+                                .slice(1)
+                                .replace(/-[\w]/g, (val, idx, str) => {
+                                  const result =
+                                    str.charAt(idx + 2) !== "-"
+                                      ? val.charAt(1).toUpperCase()
+                                      : val.charAt(1);
+                                  return " " + result;
+                                })
+                            }</a>
+                        </dd>
+                    `
+                      )
+                      .join("")}
+                       </dl> `
+                      )
+                      .join("")}
                 </div>
+                </div>
+                `;
+      });
+  }
 
-                `
-            })
-    }
-
-    attributeChangedCallback(name) {
-        this[name]();
-    }
+  attributeChangedCallback(name) {
+    this[name]();
+  }
 }
 
-customElements.define('course-footer', CourseFooter);
+customElements.define("course-footer", CourseFooter);
